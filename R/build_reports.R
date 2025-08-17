a3fcbc9cebf22daf42524d54f0bc3e086a175e84
Marwin Carmo <marwinbr@gmail.com>
2025-08-17 09:40:25 -0700

    include output reports

--- R/build_reports.R
+++ R/build_reports.R
@@ -2,6 +2,18 @@
 
 reports_to_render <- list.files("R", pattern = "\\.Rmd$")
 
+for (i in reports_to_render) {
+
+  out_name = gsub("Rmd", "pdf", i)
+
+  rmarkdown::render(
+               input = paste0("R/", i),
+               output_format = "pdf_document",
+               output_dir = "output",
+               output_file = out_name
+             )
+}
+
 rmarkdown::render(
   input = "R/02_report_unequal-sample-size.Rmd",
   output_format = "pdf_document",
